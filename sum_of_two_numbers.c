e64d0c2b0973de9e12e059d4c5edbf06b9d9e7e8
Isha Negi <itsishanegi@gmail.com>
2024-11-24 16:39:16 +0530

    Create sum_of_two_numbers.c

---
+++ sum_of_two_numbers.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+void main()
+{
+    int a,b,c;
+    printf("Enter a= ");
+    scanf("%d",&a);
+    printf("Enter b= ");
+    scanf("%d",&b);
+    c=a+b;
+    printf("The sum of a & b is: %d",c);
+    printf("\nName: Isha Negi \nRoll number: 24 CE 47\n");
+}
